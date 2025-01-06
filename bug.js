869d91150e8ecff5dd14df82f7c5bc6ec5f6829e
BugHunter-X <askzenai@gmail.com>
2025-01-06 20:52:09 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  // measurementId: "YOUR_MEASUREMENT_ID", //analytics
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const auth = getAuth();
+
+export { auth, app };
